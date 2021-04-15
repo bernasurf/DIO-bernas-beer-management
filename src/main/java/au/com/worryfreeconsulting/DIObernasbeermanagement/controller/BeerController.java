1fb89c73dd54932eec31f64da9ac6f0e53c3a7e8
Berna <bernasurfpix@gmail.com>
2021-04-16 08:44:05 +1000

    update BeerController and BeercontrollerDocs

--- src/main/java/au/com/worryfreeconsulting/DIObernasbeermanagement/controller/BeerController.java
+++ src/main/java/au/com/worryfreeconsulting/DIObernasbeermanagement/controller/BeerController.java
@@ -1,16 +1,51 @@
 package au.com.worryfreeconsulting.DIObernasbeermanagement.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import au.com.worryfreeconsulting.DIObernasbeermanagement.dto.BeerDTO;
+import au.com.worryfreeconsulting.DIObernasbeermanagement.dto.QuantityDTO;
+import au.com.worryfreeconsulting.DIObernasbeermanagement.exception.BeerAlreadyRegisteredException;
+import au.com.worryfreeconsulting.DIObernasbeermanagement.exception.BeerNotFoundException;
+import au.com.worryfreeconsulting.DIObernasbeermanagement.exception.BeerStockExceededException;
+import au.com.worryfreeconsulting.DIObernasbeermanagement.service.BeerService;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/beerstock")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class BeerController implements BeerControllerDocs {
 
-public class BeerController implements BeerControllerDocs{
+    private final BeerService beerService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BeerDTO createBeer(@RequestBody @Valid BeerDTO beerDTO) throws BeerAlreadyRegisteredException {
+        return beerService.createBeer(beerDTO);
+    }
+
+    @GetMapping("/{name}")
+    public BeerDTO findByName(@PathVariable String name) throws BeerNotFoundException {
+        return beerService.findByName(name);
+    }
 
     @GetMapping
-    public String helloAPI() {
-        return "Olá API!!";
+    public List<BeerDTO> listBeers() {
+        return beerService.listAll();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws BeerNotFoundException {
+        beerService.deleteById(id);
+    }
+
+    @PatchMapping("/{id}/increment")
+    public BeerDTO increment(@PathVariable Long id, @RequestBody @Valid QuantityDTO quantityDTO) throws BeerNotFoundException, BeerStockExceededException {
+        return beerService.increment(id, quantityDTO.getQuantity());
     }
 }
