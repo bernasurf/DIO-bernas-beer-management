daf3bf021890678b0de537986f8ae7cfd6ddaad0
Berna <bernasurfpix@gmail.com>
2021-04-15 11:47:44 +1000

    added swagger config and Readme

--- src/main/java/au/com/worryfreeconsulting/DIObernasbeermanagement/controller/BeerController.java
+++ src/main/java/au/com/worryfreeconsulting/DIObernasbeermanagement/controller/BeerController.java
@@ -1,2 +1,16 @@
-package au.com.worryfreeconsulting.DIObernasbeermanagement.controller;public class BeerController {
+package au.com.worryfreeconsulting.DIObernasbeermanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/beerstock")
+
+public class BeerController implements BeerControllerDocs{
+
+    @GetMapping
+    public String helloAPI() {
+        return "Olá API!!";
+    }
 }
--- src/main/java/au/com/worryfreeconsulting/DIObernasbeermanagement/controller/BeerControllerDocs.java
+++ src/main/java/au/com/worryfreeconsulting/DIObernasbeermanagement/controller/BeerControllerDocs.java
@@ -1,2 +1,16 @@
-package au.com.worryfreeconsulting.DIObernasbeermanagement.controller;public class BeerControllerDocs {
+package au.com.worryfreeconsulting.DIObernasbeermanagement.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api("Beer Stock Management")
+public interface BeerControllerDocs {
+
+    @ApiOperation(value = "Ready to Drink?")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Returns a simple string = Ready to Drink?")
+    })
+    String helloAPI();
 }
